8ad58a481d8fbf0ac3f15dac89071369b797f1ff
Ahmad AlHaj <ahmadhaj.2004aa@gmail.com>
2025-11-22 22:16:00 +0200

    Add StatItem widget for displaying individual statistics

--- lib/main.dart
+++ lib/main.dart
@@ -234,3 +234,39 @@ class _TypingTestPageState extends State<TypingTestPage> {
   }
 }
 
+class _StatItem extends StatelessWidget {
+  final String label;
+  final String value;
+  final IconData icon;
+
+  const _StatItem({
+    required this.label,
+    required this.value,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: Colors.blue, size: 28),
+        const SizedBox(height: 8),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[600],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
